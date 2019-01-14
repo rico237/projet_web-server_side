4da0a8767f482a68a12b287f3002e2c1f6289e15
Herrick Wolber <wolbereric@yahoo.fr>
2019-01-14 18:19:08 +0100

    Update server.js

--- init/server.js
+++ init/server.js
@@ -7,12 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('../routes/index');
 var usersRouter = require('../routes/users');
 
-
-
 app = express();
 
 // view engine setup
-
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'hbs');
 
