a286d0b1fd29703a99a526a14dae3b674e070e70
Herrick Wolber <wolbereric@yahoo.fr>
2019-01-15 14:00:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 /*
  * Import des librairies
 */
-var express 	= require('express'); 			// Librairie Server
+let express 	= require('express'); 			// Librairie Server
 let mongoose 	= require('mongoose'); 			// Gestion de mongodb
-var logger 		= require('morgan'); 			// ???
-var createError = require('http-errors'); 		// Gestion des erreurs
-var path 		= require('path'); 				// ???
+let logger 		= require('morgan'); 			// ???
+let createError = require('http-errors'); 		// Gestion des erreurs
+let path 		= require('path'); 				// ???
 let morgan 		= require('morgan');			// ???
 let bodyParser 	= require('body-parser');		// ???
 
 let app 		= express();					// Init du serveur web
-let config 		= require('config'); 			// we load the db location from the JSON files
+let config 		= require('config'); 			// Load config file
 let port 		= process.env.PORT ||  8080;	// Definition du port selon les variables de l'environnement heroku
 
 // Configuration Server                                       
@@ -27,16 +27,10 @@ app.use(function(req, res, next) {
   next();
 });
 
-
-// Routes
-var product = require('./routes/product'); // Imports routes for the products
-
 // MongoDB
 const urlmongo = process.env.MONGODB_URI;
-
 // Options recommandées par mLab pour une connexion à la base
 let options = { useMongoClient: true };
-
 // Connexion à la base mongodb
 mongoose.connect(urlmongo, options); 
 mongoose.Promise = global.Promise;		
@@ -47,6 +41,8 @@ db.once('open', () => {
     console.log("Connexion à la base établie avec succès"); 
 });
 
+// Routes
+let product = require('./routes/product'); // Imports routes for the products
 
 // Endpoints
 app.use("/products", product);
