82966b5b407120b7389937789ed50193f91450a1
Rico <wolbereric@yahoo.fr>
2019-01-21 13:53:37 +0100

    Update product.js

--- controllers/product.js
+++ controllers/product.js
@@ -42,7 +42,7 @@ exports.product_find_all = function () {
     })
 };
 
-exports.product_details = function (req, res) {
+exports.product_details = function (req, res, next) {
     Product.findById(req.params.id, function (err, product) {
         if (err) return next(err);
         res.send(product);
