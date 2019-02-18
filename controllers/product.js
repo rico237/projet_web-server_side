117e3e008f0911951b48aa1f5d2d06c632b079da
Rico <wolbereric@yahoo.fr>
2019-02-18 17:45:56 +0100

    Update product.js

--- controllers/product.js
+++ controllers/product.js
@@ -73,7 +73,7 @@ exports.find_images_url_for_product = function(req, res, next) {
 }
 
 exports.update_product_image = function(req, res, next) {
-    Product.findByIdAndUpdate(ObjectId(req.params.id), { $set: {product_image_url : req.body.imageUrl }}, function (err, product) {
+    Product.findByIdAndUpdate(req.params.id, { $set: {product_image_url : req.body.imageUrl }}, function (err, product) {
         if (err) return next(err);
         res.send('Product udpated.');
     });
@@ -116,19 +116,19 @@ exports.find_ingredients_from_products_with_allergens = function(req, res, next)
 };
 
 exports.product_additives = function(req, res, next) {
-    Product.findById(ObjectId(req.params.id), "additives_tags -_id").lean().exec()
+    Product.findById(req.params.id, "additives_tags -_id").lean().exec()
         .then(additives => res.status(200).json({additives}))
         .catch(err => next(err))
 };
 
 exports.product_ingredients = function(req, res, next) {
-    Product.findById(ObjectId(req.params.id), "ingredients -_id").lean().exec()
+    Product.findById(req.params.id, "ingredients -_id").lean().exec()
         .then(ingredients => res.status(200).json({ingredients}))
         .catch(err => next(err))
 }
 
 exports.product_ingredients_description = function(req, res, next) {
-    Product.findById(ObjectId(req.params.id), "ingredients_text_with_allergens -_id").lean().exec()
+    Product.findById(req.params.id, "ingredients_text_with_allergens -_id").lean().exec()
         .then(ingredients => res.status(200).json({ingredients}))
         .catch(err => next(err))
 }
@@ -140,21 +140,21 @@ exports.product_find_all = function (req, res, next) {
 };
 
 exports.product_details = function (req, res, next) {
-    Product.findById( ObjectId(req.params.id) , function (err, product) {
+    Product.findById( req.params.id , function (err, product) {
         if (err) return next(err);
         res.send(product);
     })
 };
 
 exports.product_update = function (req, res, next) {
-    Product.findByIdAndUpdate(ObjectId(req.params.id), {$set: req.body}, function (err, product) {
+    Product.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, product) {
         if (err) return next(err);
         res.send('Product udpated.');
     });
 };
 
 exports.product_delete = function (req, res, next) {
-    Product.findByIdAndRemove(ObjectId(req.params.id), function (err) {
+    Product.findByIdAndRemove(req.params.id, function (err) {
         if (err) return next(err);
         res.send('Deleted successfully!');
     })
