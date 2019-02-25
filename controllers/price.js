ba3cf31b52084964b566b651fc44438cc7e1527b
haneul <kehoecicco@live.fr>
2019-02-25 09:46:40 +0100

    hello

--- controllers/price.js
+++ controllers/price.js
@@ -3,7 +3,9 @@ let ObjectId = require('mongoose').Types.ObjectId;
 
 
 exports.get_all_prices = function(req, res, next) {
-    
+
+    console.log(req.params.id);
+
     Price.find({
             id_product: ObjectId(req.params.id)
         }).lean().exec()
